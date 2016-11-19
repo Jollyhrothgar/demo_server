302654c5c05a2d025f41381fe1e8d9b7b1daf9d2
Mike Beaumier <michael.beaumier@gmail.com>
2016-11-18 17:31:51 -0800

    use gitlab API + git hooks to automatically search for and clone modules checked into the demos group. Additionally, webhook triggers a re-clone every time there is a push

--- run_demo_framework_server.py
+++ run_demo_framework_server.py
@@ -6,6 +6,8 @@ import os
 import shutil
 import json
 import subprocess
+import shlex
+from urllib.request import urlopen
 
 # Framework
 
@@ -16,7 +18,11 @@ PORT = 6969
 STATIC = os.path.join(os.path.dirname(__file__),'demo_framework_server')
 TEMPLATES = os.path.join(os.path.dirname(__file__),'demo_framework_server/templates')
 DEMO_DIR = '/home/mike/workspace/demo_framework/demos'
+API_TOKEN = '_9BHn9oy7zGvoM-RRziy'
+API_URL = 'http://10.0.1.10/api/v3/projects/all?private_token={}'
+API_URL = API_URL.format(API_TOKEN)
 app = flask.Flask(__name__, static_folder=STATIC, template_folder=TEMPLATES)
+demos = []
 
 # FLASK APPLICATION ROUTES ####################################################
 @app.route('/favicon.ico')
@@ -62,21 +68,44 @@ def recreate_demo(project_name):
     return
 
 def clear_demos():
+    global demos
+    demos = []
+    print("Emptying the demos area")
     try:
         shutil.rmtree(DEMO_DIR)
         shutil.os.mkdir(DEMO_DIR)
     except:
         shutil.os.mkdir(DEMO_DIR)
+    print("Ready for Demos")
+    return
+
+def get_demos():
+    """
+    Uses the GitLab API to get a full repository listing and extract the projects
+    which belong to the demos group.
+    """
+    global demos
+    all_projects = json.loads(urlopen(API_URL).read().decode())
+    for project in all_projects:
+        if project['namespace']['name'] == 'demos':
+            tokens = project['name_with_namespace'].split()
+            demos.append(tokens[2]) # third token is project name
+            print("DEMO FOUND: ", project['path_with_namespace'])
+    for demo in demos:
+        recreate_demo(demo)
+    return
+
 # END HELPER FUNCTIONS ########################################################
 
 # UNIT TESTS ##################################################################
 # END UNIT TESTS ##############################################################
 
-def startup_tasks():
+def reset():
     clear_demos()
+    get_demos()
 
 if __name__ == "__main__":
-    startup_tasks()
+    reset()
     app.run(
         host=HOSTNAME,
         port=PORT,
