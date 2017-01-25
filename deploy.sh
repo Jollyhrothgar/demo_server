0f63963cb51a0c68f29a9805689ba43c3808f952
Mike Beaumier <michael.beaumier@gmail.com>
2017-01-24 17:12:53 -0800

    removed commented code from deploy and kill. Also fixed a minor bug with running interactively locally. It works again

--- deploy.sh
+++ deploy.sh
@@ -8,30 +8,4 @@ for demo in `find demos -maxdepth 2 | grep 'main.py'`; do
 	python $demo &
 done
 
-# This is a fancier version for when/if demo modules will support
-# independant their own virtual environments
-
-#for d in */ ; do
-	##Find if theres a main.py in this directory
-	#if [ -f $d/main.py ]
-	#then
-		#echo "Found a main.py in $d"
-
-		##Check if theres a virtual environment to source before we can deploy
-		#if [ -d $d/venv ]
-		#then
-			#echo "Found a virtual environment in $d"
-			#env = true
-			#source $d/venv/bin/activate
-		#fi
-
-		##Deploy!
-		#python $d/main.py &
-
-		#if [ "$env" = true ]
-		#then
-			#deactivate
-			#unset env
-		#fi
-	#fi
-#done
+# add a means to spawn virtualenvs later. The previous means would not have worked.
