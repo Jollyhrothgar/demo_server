07f4540a7dad1e67ddbd7e4f405d41537a96ff2d
Martin Voigt <martin.voigt@daimler.com>
2017-04-07 15:02:24 -0700

    bandaid fix on the module name detection, replace with uuid

--- mlpux_demo/setup.py
+++ mlpux_demo/setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup( 
     name='mlpux',
     package_dir = {'':'mlpux'},
-    version='0.1.0',
+    version='1.0.0',
     author='Mike Beaumier',
     author_email='michael.beaumier@gmail.com',
     py_modules=['mlpux'],
